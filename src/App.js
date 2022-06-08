90b7723cbe57e8188ea89f71aa3e022e49240b4a
brunomartin <bruno.pedraza@conexa.ai>
2022-06-08 16:35:04 -0300

    use services API, convert to points

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import * as tt from '@tomtom-international/web-sdk-maps'
+import * as ttapi from '@tomtom-international/web-sdk-services'
 import './App.css';
 import '@tomtom-international/web-sdk-maps/dist/maps.css'
 
@@ -9,7 +10,33 @@ const App = () =>{
   const [longitude, setLongitude]= useState(-57.5458946)
   const [latitude, setLatitude]= useState(-38.0420353)
 
+  const convertToPoints = (lngLat) => {
+    return{
+      point:{
+        latitude: lngLat.lat,
+        longitude: lngLat.lng
+      }
+    }
+  }
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div')
+    element.className = 'marker-delivery'
+    new tt.Marker({
+      element: element
+    })
+    .setLngLat(lngLat)
+    .addTo(map)
+  }
+
   useEffect(() =>{
+    const origin = {
+      lng: longitude,
+      lat: latitude,
+    }
+
+    const destinations = []
+
     let map = tt.map({
       key: 'kdZtBKzO9Z1ih7uVApCyfuI0GLslsBEm',
       container: mapElement.current,
@@ -26,7 +53,7 @@ const App = () =>{
       const popupOffset = {
         bottom: [0, -25]
       }
-      const popup = new tt.Popup({ offeset:popupOffset }).setHTML('This is you!')
+      const popup = new tt.Popup({ offset:popupOffset }).setHTML('This is you!')
       const element = document.createElement('div')
       element.className= 'marker'
 
@@ -46,6 +73,30 @@ const App = () =>{
       marker.setPopup(popup).togglePopup()
     }
     addMarker()
+
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+
+        const callParameters = {
+          key: 'kdZtBKzO9Z1ih7uVApCyfuI0GLslsBEm',
+          destinations: pointsForDestinations,
+          origin: [convertToPoints(origin)]
+        }
+      
+    }
+
+
+    // return new Promise((resolve, reject) =>{
+    //   ttapi.services
+    //   .matrixRouting(callParameters)
+
+    //? })
+    map.on('click', (e) =>{
+      destinations.push(e.lngLat)
+      addDeliveryMarker(e.lngLat, map)
+    })
 
     return () => map.remove()
   }, [longitude, latitude])
